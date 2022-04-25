c862a5dc09271ade5f7dbca57f273872935bec7d
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-04-25 18:22:21 +0200

    refactor test with setup

--- php/tests/Unit/AccountTest.php
+++ php/tests/Unit/AccountTest.php
@@ -7,53 +7,52 @@ use PHPUnit\Framework\TestCase;
 
 class AccountTest extends TestCase
 {
+    private $account;
+
+    public function setUp(): void
+    {
+        $this->account = new Account();
+    }
+
     /** @test */
     public function should_not_deposit_negative_money(): void
     {
         $this->expectException(\InvalidArgumentException::class);
-        $account = new Account();
-        $account->deposit(-1000);
+        $this->account->deposit(-1000);
     }
 
     /** @test */
     public function should_not_deposit_zero_money(): void
     {
         $this->expectException(\InvalidArgumentException::class);
-        $account = new Account();
-        $account->deposit(0);
+        $this->account->deposit(0);
     }
 
     /** @test */
     public function should_deposit_money(): void
     {
-        $account = new Account();
-        $account->deposit(300);
-
-        $this->assertTrue($account->transactions[0] === 300);
+        $this->account->deposit(300);
+        $this->assertTrue($this->account->transactions[0] === 300);
     }
 
     /** @test */
     public function should_not_withdraw_zero_money(): void
     {
         $this->expectException(\InvalidArgumentException::class);
-        $account = new Account();
-        $account->withdraw(0);
+        $this->account->withdraw(0);
     }
 
     /** @test */
     public function should_not_withdraw_negative_money(): void
     {
         $this->expectException(\InvalidArgumentException::class);
-        $account = new Account();
-        $account->withdraw(-1000);
+        $this->account->withdraw(-1000);
     }
 
     /** @test */
     public function should_withdraw_money(): void
     {
-        $account = new Account();
-        $account->withdraw(300);
-
-        $this->assertTrue($account->transactions[0] === -300);
+        $this->account->withdraw(300);
+        $this->assertTrue($this->account->transactions[0] === -300);
     }
 }
