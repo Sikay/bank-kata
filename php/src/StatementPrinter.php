dd7650e70eb517d57da637127d82c7b9becbb989
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-04-26 23:16:50 +0200

    refactor

--- php/src/StatementPrinter.php
+++ php/src/StatementPrinter.php
@@ -4,20 +4,38 @@ namespace BankKata;
 
 class StatementPrinter
 {
+    private const STATEMENT_HEADER = 'Date       || Amount || Balance';
+
     public function print(array $transactions): void
     {
-        $header = 'Date       || Amount || Balance' . PHP_EOL;
-        $body = '';
+        $this->printHeader();
+        $this->printStatement($transactions);
+    }
+
+    private function printHeader(): void
+    {
+        print(self::STATEMENT_HEADER . PHP_EOL);
+    }
+
+    private function printStatement(array $transactions): void
+    {
+        $statement = '';
+        $totalAmount = $this->calculateTotalAmount($transactions);
+        $transactionShort = array_reverse($transactions);
+        foreach ($transactionShort as $transaction) {
+            $statement .= $transaction->date() . ' || ' . $transaction->amount() . '   || ' . $totalAmount . PHP_EOL;
+            $totalAmount -= $transaction->amount();
+        }
+
+        print($statement);
+    }
+
+    private function calculateTotalAmount(array $transactions): int
+    {
         $totalAmount = 0;
         foreach ($transactions as $transaction) {
             $totalAmount += $transaction->amount();
         }
-        $transactionShort = array_reverse($transactions);
-        foreach ($transactionShort as $transaction) {
-            $body .= $transaction->date() . ' || ' . $transaction->amount() . '   || ' . $totalAmount . PHP_EOL;
-            $totalAmount -= $transaction->amount();
-        }
-        $statement = $header . $body;
-        print($statement);
+        return $totalAmount;
     }
 }
