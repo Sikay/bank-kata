396d2db5da7a5eedc3211ea3ef1d77111fbde9c1
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-04-26 12:26:21 +0200

    should_add_transaction_in_the_same_order

--- php/src/Transaction.php
+++ php/src/Transaction.php
@@ -4,8 +4,24 @@ namespace BankKata;
 
 class Transaction
 {
+    private $date;
+    private $amount;
+
     public function __construct(string $date, int $amount)
     {
-
+        $this->date = $date;
+        $this->amount = $amount;
     }
+
+    public function amount(): int
+    {
+        return $this->amount;
+    }
+
+    public function date(): string
+    {
+        return $this->date;
+    }
+
+
 }
--- php/tests/Unit/TransactionsTest.php
+++ php/tests/Unit/TransactionsTest.php
@@ -12,9 +12,39 @@ class TransactionsTest extends TestCase
     public function should_add_transaction(): void
     {
         $transactions = new Transactions();
-        $transaction = new Transaction('', 200);
+        $transaction = new Transaction('14/02/2017', 200);
         $transactions->add($transaction);
 
         $this->assertTrue(sizeof($transactions->all()) === 1);
+    }
+
+    /** @test */
+    public function should_fail_if_add_transaction_in_different_order(): void
+    {
+        $transactions = new Transactions();
+        $deposit = new Transaction('14/02/2017', 400);
+        $withdraw = new Transaction('15/02/2017', -200);
+
+        $transactions->add($deposit);
+        $transactions->add($withdraw);
+
+        $this->assertTrue(sizeof($transactions->all()) === 2);
+        $this->assertNotSame($transactions->all()[0], $withdraw);
+        $this->assertNotSame($transactions->all()[1], $deposit);
+    }
+
+    /** @test */
+    public function should_add_transaction_in_the_same_order(): void
+    {
+        $transactions = new Transactions();
+        $deposit = new Transaction('14/02/2017', 400);
+        $withdraw = new Transaction('15/02/2017', -200);
+
+        $transactions->add($deposit);
+        $transactions->add($withdraw);
+
+        $this->assertTrue(sizeof($transactions->all()) === 2);
+        $this->assertSame($transactions->all()[0], $deposit);
+        $this->assertSame($transactions->all()[1], $withdraw);
     }
 }
